bd7f93f65cfeaba67d8f1c5bafbecb58998571e6
Igor Padovan da Silva <igorpadovan@gmail.com>
2021-02-23 11:50:28 -0400

    init

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,13 @@
+export function ExperienceBar() {
+    return(
+        <header className="experience-bar">
+            <span>8px</span>
+            <div>
+                <div style={{ width: '50%' }}>
+                    <span className="current-expirience" style={{ left: '50%' }}>300xp</span>
+                </div>
+            </div>
+            <span>600px</span>
+        </header>
+    );
+}
