219098cfbbae212997989a7d0e9d774685ee4106
Igor Padovan da Silva <igorpadovan@gmail.com>
2021-02-23 21:23:19 -0400

    migrating to nextjs

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,10 +1,12 @@
+import styles  from '../styles/components/ExperienceBar.module.css'
+
 export function ExperienceBar() {
     return(
-        <header className="experience-bar">
+        <header className={styles.experienceBar}>
             <span>8px</span>
             <div>
                 <div style={{ width: '50%' }}>
-                    <span className="current-expirience" style={{ left: '50%' }}>300xp</span>
+                    <span className={styles.currentExperience} style={{ left: '50%' }}>300xp</span>
                 </div>
             </div>
             <span>600px</span>
