6c857045c9d01fc62b8f4f4017b1b7386947bcd5
Igor Padovan da Silva <igorpadovan@gmail.com>
2021-02-24 21:13:58 -0400

    start countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,28 @@
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import styles from "../styles/pages/Home.module.css"
+import Head from "next/head"
 
 export default function Home() {
-  return (
+  return (      
     <div className={styles.container}>
+      <Head>
+        <title>Pomodoro Fitness</title>
+      </Head> 
       <ExperienceBar />
+
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
-        <div>
+        <div>   
 
         </div>
-      </section>
-      
+      </section>      
     </div>
   )
 }
